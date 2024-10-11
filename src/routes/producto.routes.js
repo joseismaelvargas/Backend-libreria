e3522b173d5edb1e2e6812167f8e237b4a8c0584
ismael <134803262+joseismaelvargas@users.noreply.github.com>
2024-10-10 21:12:40 -0300

    Update producto.routes.js

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -7,6 +7,6 @@ const ruta=Router();
 ruta.route('/prueba').get(prueba)
 ruta.route('/productos').post(crearproducto)
 ruta.route('/productolista').post(crearproducto).get(listaProducto)
-ruta.route('/productos/:id').put(editarProducto)
+
 ruta.route('/productos/:id').put(editarProducto).delete(deleteProducto)
 export default ruta
